0d0c685bf341b3bb6f4c4755170b9885cdc6604a
Dale Ranta <righthalfplane@cox.com>
2020-01-09 09:04:02 -0800

    Going to freeglut

--- Glut/build-debug.bat
+++
@@ -1,6 +0,0 @@
-rm *.o *.a
-i686-w64-mingw32-gcc -DWIN32 -D_WINDOWS -D_USRDLL -g -O0 -c *.c -I../include
-i686-w64-mingw32-ar rcv libglut32-debug.a *.o
-mv libglut32-debug.a ../lib/
-rm *.o *.a
-
--- Glut/build64.bat
+++
@@ -1,6 +0,0 @@
-del *.o *.a
-x86_64-w64-mingw32-gcc -m64  -DWIN32 -D_WINDOWS -D_USRDLL -c *.c -I../include
-x86_64-w64-mingw32-ar rcv libglut64.a *.o
-move /Y libglut64.a ../lib/
-del *.o *.a
-
