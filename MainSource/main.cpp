2eccdd64232d5197ce3738ee97285ab59ec3c345
Dale Ranta <righthalfplane@cox.com>
2020-02-23 09:08:12 -0800

    Add rtaudio to project list

--- MainSource/main.cpp
+++ MainSource/main.cpp
@@ -34,7 +34,7 @@ static void control_cb(int control);
 
 static void control_cb2(int control);
 
-char *ProgramVersion=(char *)"SdrGlut-521";
+char *ProgramVersion=(char *)"SdrGlut-522";
 
 extern "C" struct Scene *sceneRoot(void);
 
