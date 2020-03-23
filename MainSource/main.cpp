7286ab65a1fb53d0cc395aaa920e91e934ebf3eb
Dale Ranta <righthalfplane@cox.com>
2020-03-22 20:34:49 -0700

    Added Version Number

--- MainSource/main.cpp
+++ MainSource/main.cpp
@@ -34,7 +34,7 @@ static void control_cb(int control);
 
 static void control_cb2(int control);
 
-char *ProgramVersion=(char *)"SdrGlut-524";
+char *ProgramVersion=(char *)"SdrGlut-525";
 
 extern "C" struct Scene *sceneRoot(void);
 
