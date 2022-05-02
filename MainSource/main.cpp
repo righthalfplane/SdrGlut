cb1b63a3a54a11b5ce0195bf564bd441837c4fb9
Dale Ranta <righthalfplane@cox.com>
2022-05-02 16:56:43 -0700

    audio2

--- MainSource/main.cpp
+++ MainSource/main.cpp
@@ -39,7 +39,7 @@ static void control_cb(int control);
 
 static void control_cb2(int control);
 
-char *ProgramVersion=(char *)"SdrGlut-979";
+char *ProgramVersion=(char *)"SdrGlut-980";
 
 extern "C" struct Scene *sceneRoot(void);
 
