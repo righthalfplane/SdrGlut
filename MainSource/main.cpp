9c6ff85fc0e9b0b7cca91dcb6a9c5183bd3a3954
Dale Ranta <righthalfplane@cox.com>
2022-08-19 18:29:05 -0700

    remotespeakers 12

--- MainSource/main.cpp
+++ MainSource/main.cpp
@@ -39,7 +39,7 @@ static void control_cb(int control);
 
 static void control_cb2(int control);
 
-char *ProgramVersion=(char *)"SdrGlut-1118";
+char *ProgramVersion=(char *)"SdrGlut-1119";
 
 extern "C" struct Scene *sceneRoot(void);
 
