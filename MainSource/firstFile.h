540f442480bd03e81ab23dfee9f2d1a1bd9f2b01
Dale Ranta <righthalfplane@cox.com>
2023-11-20 15:33:14 -0800

    RtAudio - Ubuntu

--- MainSource/firstFile.h
+++ MainSource/firstFile.h
@@ -11,17 +11,8 @@
 #ifndef __FIRSTFILE_H__
 #define __FIRSTFILE_H__
 
-
 #ifndef  _CRT_SECURE_NO_DEPRECATE
 #define _CRT_SECURE_NO_DEPRECATE 1
-#endif
-
-#if LIQUID_VERSION_NUMBER >= 1003001 || __arm__
-#define LIQUID_VERSION_4 1
-#endif
-
-#if LIQUID_VERSION_NUMBER >= 1003002
-#define WINDOWS_LONG_NAMES 1
 #endif
 
 #endif
